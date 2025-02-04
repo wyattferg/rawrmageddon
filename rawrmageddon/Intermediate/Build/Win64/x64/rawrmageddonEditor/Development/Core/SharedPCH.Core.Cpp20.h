1f3e4699be421a039d599bc754f0d214edd21c86
andrewoh365 <andrewoh365@gmail.com>
2025-02-04 10:04:10 -0600

    Added light switch functionality

---
+++ rawrmageddon/Intermediate/Build/Win64/x64/rawrmageddonEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/andre/OneDrive/Documents/Andrew_Senior/VIST_487/rawrmageddon/rawrmageddon/Intermediate/Build/Win64/x64/rawrmageddonEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ rawrmageddon/Intermediate/Build/Win64/x64/rawrmageddonEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/andre/OneDrive/Documents/Andrew_Senior/VIST_487/rawrmageddon/rawrmageddon/Intermediate/Build/Win64/x64/rawrmageddonEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
